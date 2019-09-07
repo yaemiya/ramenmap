1a4640eccc534ed451e1f2975bd7d215ab66c2e1
yaemiya <53666797+yaemiya@users.noreply.github.com>
2019-09-07 13:16:29 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -21,8 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
-
+require __DIR__.'/../../laravel/ramenmap/vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +34,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../../laravel/ramenmap/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
